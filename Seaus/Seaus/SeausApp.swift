02cfda3950e64b53c3f00723847afb98ca70b134
YashB-byte <yash.behera@icloud.com>
2026-05-23 13:09:18 -0700

    small improvements

---
+++ Seaus/Seaus/SeausApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeausApp.swift
+//  Seaus
+//
+//  Created by Yash Behera on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SeausApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
